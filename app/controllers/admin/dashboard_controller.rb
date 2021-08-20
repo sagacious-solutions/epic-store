2a7e58931c0f2082a279e4f30cd620903e49f93b
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-20 15:46:46 -0700

    added instance variables for product and catagory counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: []
 
   def show
+    @product_count = Product.count
+    @catagory_count = Category.count
   end
 end
