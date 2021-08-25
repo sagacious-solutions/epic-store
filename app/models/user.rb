2ad974e4111d06870d309edf85441360d2b19657
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-25 15:23:41 -0700

    added validations
    added validations for name, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
+
+  validates :password, :presence => true,
+  :confirmation => true,
+  :length => {:within => 6..40},
+  :on => :create
+
 end
