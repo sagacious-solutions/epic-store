6360d2bdd4d76bb128dc9e4c87e741fa3e25f391
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-25 16:35:56 -0700

    tests for authenticate_with_credentials
    added tests for invalid inputs and valid but spaced email and case insensitivity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ require 'user'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     before do 
-      @user = User.new({:first_name => "Steve", :last_name => "Mcneely", :email => "hotmale@hotmail.com", password:"somejunk", password_confirmation:"somejunk"})
+      @user = User.new({:first_name => "Steve", :last_name => "Mcneely", :email => "stdfgdkfkljgjnm@hotmail.com", password:"somejunk", password_confirmation:"somejunk"})
     end    
 
 
@@ -38,5 +38,40 @@ RSpec.describe User, type: :model do
       expect(existing_user).to be_nil
     end
 
+    it "should not create a user if the passwords don't match" do
+      bad_user = User.new({:first_name => "Steve", :last_name => "Mcneely", :email => "steeeeeeeve@hotmail.com", password:"somejunk", password_confirmation:"badpassword"})
+      
+      expect(bad_user.save).to be_falsey
+    end
+    
   end
+
+   describe '.authenticate_with_credentials' do
+    before do
+      User.find_by_email("dsfdf@hotmail.com").destroy
+      @new_user = User.new({:first_name => "Steve", :last_name => "Mcneely", :email => "dsfdf@hotmail.com", password:"somejunk", password_confirmation:"somejunk"})
+      @new_user.save
+    end
+
+    it "should return the user if their credentials are valid" do
+      expect(@new_user.authenticate_with_credentials("dsfdf@hotmail.com", "somejunk")).to eq(@new_user)
+    end
+
+    it "should return the false if their credentials are invalid" do
+      expect(@new_user.authenticate_with_credentials("dsfdf@hotmail.com", "wrongpassword")).to be_falsey
+    end
+
+    it "should return the user if their credentials are valid but have leading and trailing whitespace in email" do
+      expect(@new_user.authenticate_with_credentials("  dsfdf@hotmail.com   ", "somejunk")).to eq(@new_user)
+    end
+
+    it "should return the user if their case doesn't match the case of the email on record" do
+      expect(@new_user.authenticate_with_credentials("  DsFdf@HotMAIL.com   ", "somejunk")).to eq(@new_user)
+    end
+
+    after do 
+      User.find_by_email("dsfdf@hotmail.com").destroy
+    end  
+
+   end
 end
