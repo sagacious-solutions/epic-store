e82d966a7d571ef5b5acf807612f629609b9e776
Matt Macdonald <mksmacdonald@gmail.com>
2021-08-25 15:22:52 -0700

    bug fix - redriect on registration failure
    If the registration didn't pass validations, it was redirecting to the wrong path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/registration_path'
+      redirect_to '/registration'
     end
   end   
 
